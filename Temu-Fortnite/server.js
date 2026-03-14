30910720e082cff4a29236f83ff78b2669d855c4
thenils projects <malviyanilay@gmail.com>
2026-03-14 18:34:06 +0300

    Set server to listen on specified port

--- Temu-Fortnite/server.js
+++ Temu-Fortnite/server.js
@@ -1,3 +1,5 @@
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server active on port ${PORT}`));
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
